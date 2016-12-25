8abd0981da97e96bcd7470bfbbe24bee6d65cf64
Andrew Shadura <andrew@shadura.me>
2016-12-25 11:56:38 +0100

    Drop old code from __init__.py.

--- crecord/__init__.py
+++ crecord/__init__.py
@@ -6,24 +6,3 @@
 # the GNU General Public License, incorporated herein by reference.
 #
 # Much of this extension is based on Bryan O'Sullivan's record extension.
-
-'''text-gui based change selection during commit'''
-from .crecord_core import dorecord
-
-def crecord(ui, repo, *pats, **opts):
-    '''interactively select changes to commit
-
-    If a list of files is omitted, all changes reported by :hg:`status`
-    will be candidates for recording.
-
-    See :hg:`help dates` for a list of formats valid for -d/--date.
-
-    You will be shown a list of patch hunks from which you can select
-    those you would like to apply to the commit.
-
-    This command is not available when committing a merge.'''
-
-    dorecord(ui, repo, None, *pats, **opts)
-
-testedwith = '3.0.2 3.1.2 3.2.4 3.3.3 3.4.3 3.5.2 3.6'
-buglink = 'https://bitbucket.org/edgimar/crecord/issues'
