30c7f4cb3d2f25c45a66498b93566b313f346c26
Andrew Shadura <andrew@shadura.me>
2017-01-04 18:56:45 +0100

    Generate manpages at the build time, not when the file is executed.
    This also removes the unconditional runtime dependency on docutils.

--- setup.py
+++ setup.py
@@ -10,8 +10,11 @@ def read(fname):
 def glob(fname):
     return fnmatch.filter(os.listdir(os.path.dirname(__file__)), fname)
 
-def generate_manpage(fname):
+def generate_manpage(src, dst):
     import docutils.core
+    docutils.core.publish_file(source_path=src, destination_path=dst, writer_name='manpage')
+
+def man_name(fname):
     import re
     matches = re.compile(r'^:Manual section: *([0-9]*)', re.MULTILINE).search(read(fname))
     if matches:
@@ -20,7 +23,6 @@ def generate_manpage(fname):
         section = '7'
     base = os.path.splitext(fname)[0]
     manfname = base + '.' + section
-    docutils.core.publish_file(source_path=fname, destination_path=manfname, writer_name='manpage')
     return manfname
 
 def man_path(fname):
@@ -28,7 +30,7 @@ def man_path(fname):
     return os.path.join('share', 'man', 'man' + category), [fname]
 
 def man_files(pattern):
-    return map(man_path, map(generate_manpage, glob(pattern)))
+    return map(man_path, map(man_name, glob(pattern)))
 
 # monkey patch setuptools to use distutils owner/group functionality
 from setuptools.command import sdist
@@ -38,6 +40,17 @@ class sdist_new(sdist_org):
         sdist_org.initialize_options(self)
         self.owner = self.group = 'root'
 sdist.sdist = sdist_new
+
+__manpages__ = 'git-*.rst'
+
+from setuptools.command import build_py
+build_py_org = build_py.build_py
+class build_py_new(build_py_org):
+    def run(self):
+        build_py_org.run(self)
+        if not self.dry_run:
+            map(lambda x: generate_manpage(*x), map(lambda x: (x, man_name(x)), glob(__manpages__)))
+build_py.build_py = build_py_new
 
 __name__ = "git-crecord"
 
@@ -51,7 +64,7 @@ setup(
     long_description = read('README.rst'),
     license = 'GPL-2+',
     packages = find_packages(),
-    install_requires = ['docutils>=0.12'],
+    setup_requires = ['docutils>=0.12'],
     classifiers = [
         'Development Status :: 4 - Beta',
         'Environment :: Console :: Curses',
@@ -67,7 +80,7 @@ setup(
         (os.path.join('share', 'doc', __name__), glob('*.rst')),
         (os.path.join('share', 'doc', __name__), glob('*.png')),
         (os.path.join('share', 'doc', __name__), ['CONTRIBUTORS', 'COPYING'])
-    ] + man_files('git-*.rst'),
+    ] + man_files(__manpages__),
     entry_points = {
         'console_scripts': [
             'git-crecord = git_crecord.main:main'
